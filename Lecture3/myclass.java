db1082fab62fd54fa23b47391fd033f15f2f05a3
Niki Ramanchev <147237222+creamfl@users.noreply.github.com>
2023-10-18 18:20:35 +0200

    Update myclass.java

--- Lecture3/myclass.java
+++ Lecture3/myclass.java
@@ -1,1 +1,25 @@
+class myNumber{
+  private int num;
+  public int num2;
+  public myNumber(){
+    num=0;
+    num2=100;
+}
+int getNum(){
+  return num;
+}
+void setNum(int i){
+    num=i; 
+    num2=10*i;
+  }
+}
 
+public class myclass {
+  public static void main(String[]ar){
+      myNumber A=new myNumber();
+      System.out.println(A.getNum());
+      A.setNum(10);
+      System.out.println(A.getNum());
+      System.out.println(A.num2); 
+  }
+}
