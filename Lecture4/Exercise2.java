eacf5b1350bca34e995b2e99d3ca97373091b968
Niki Ramanchev <147237222+creamfl@users.noreply.github.com>
2023-10-25 19:09:10 +0200

    Create Exercise2.java

---
+++ Lecture4/Exercise2.java
@@ -0,0 +1,29 @@
+import java.lang.Math;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		double decimalVariable = 6;
+	       
+        double tangentValue = Math.tan(decimalVariable);
+        double cotangentValue = 1 / tangentValue;
+        double circleRadius = decimalVariable;
+        double circleArea = Math.PI * Math.pow(circleRadius, 2);
+        
+
+     
+        System.out.println("Sine: " + Math.sin(decimalVariable));
+        System.out.println("Cosine: " + Math.cos(decimalVariable));
+        System.out.println("Tangent: " + Math.tan(decimalVariable));
+        System.out.println("Cotangent: " + 1 / tangentValue );
+        System.out.println("Square Root: " + Math.sqrt(decimalVariable));
+        System.out.println("Square: " + Math.pow(decimalVariable, 2));
+        System.out.println("Cube: " + Math.pow(decimalVariable, 3) );
+        System.out.println("Smallest Integer Larger or Equal: " + Math.ceil(decimalVariable));
+        System.out.println("Largest Integer Smaller or Equal: " + Math.floor(decimalVariable) );
+        System.out.println("Area of the Circle: " + circleArea );
+        System.out.println("Value of Pi: " + Math.PI);
+        System.out.println("Value of E: " + Math.E);
+	}
+
+}
